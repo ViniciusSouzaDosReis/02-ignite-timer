bfd8f7cb09516d24388cf5fb21662bc52c8174b8
Vinicius Reis <vinireis010305@gmail.com>
2023-08-16 11:24:09 -0300

    Reducers - Salvando estado no storage

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -22,7 +22,7 @@ export function Countdown() {
       interval = setInterval(() => {
         const secondsDifference = differenceInSeconds(
           new Date(),
-          currentCycle.startDate,
+          new Date(currentCycle.startDate),
         )
 
         if (secondsDifference >= totalSeconds) {
@@ -58,7 +58,6 @@ export function Countdown() {
       document.title = `${minutes}:${seconds}`
     }
   }, [minutes, seconds, currentCycle])
-
 
   return (
     <CountdownContainer>
